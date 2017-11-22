688d82a6ba098732db4ede6a79b101acd76a5bb2
Kostya Dmitrijev <kdmitrjev@gmail.com>
2017-11-22 11:05:46 +0200

    ошибка 404
    ошибка 404

--- lot.php
+++ lot.php
@@ -4,13 +4,11 @@ require_once ('functions.php');
 require_once ('users_lots.php');
 require_once ('data.php');
 
-/*if (isset ($_GET['id'])){
-$id=$_GET['id'];
-if($id>count($_GET['id']) OR $id<0) {
-    header("Location: /template/404.php");
-}
-}
-*/
+$id=$_GET['id'] ??  null;
+if($id>=count($lot_data)) {
+    http_response_code(404);
+}else{
+
 $user=user_status($is_auth, $user_name, $user_avatar);
 
 $list_menu = nav_list_menu($cat);
@@ -21,5 +19,5 @@ $lot=print_lot($lot_data, $bets);
 $page_content.=$lot;
 
 $layout_content=include_template('layout.php', ['page_title'=>'Лот','auth_user' =>$user, 'nav'=>$nav_menu,'content'=>$page_content, 'auth'=>$is_auth,'name'=>$user_name, 'avatar'=>$user_avatar]);
-$layout_content=preg_replace('<main class="container">' , 'main',$layout_content);
+$layout_content=preg_replace('<main class="container">' , 'main',$layout_content);}
 print($layout_content);
