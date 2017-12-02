5218c16b1c9aca75ea6e1b25cdc2e6b0fe6612e1
Kostya Dmitrijev <kdmitrjev@gmail.com>
2017-12-02 11:30:36 +0200

    список лотов
    Список
    Авторизация

---
+++ template/my-lots.php
@@ -0,0 +1,12 @@
+<nav class="nav">
+    <ul class="nav__list container">
+        <?= $list_menu; ?>
+    </ul>
+</nav>
+
+<section class="rates container">
+    <h2>Мои ставки</h2>
+    <table class="rates__list">
+      <?=$added_lot?>
+    </table>
+  </section>
