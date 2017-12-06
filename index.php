dea43dadbce0873bb9ce375bd00b73c6ca965a86
Kostya Dmitrijev <kdmitrjev@gmail.com>
2017-12-06 12:41:03 +0200

    начало запросов

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <?php
 
 require_once('functions.php');
-require_once('data.php');
 require_once('users_lots.php');
 require_once('init.php');
 
 session_start();
 $auth_status = include_template('non_auth_user.php', []);
-if (isset($_SESSION['user'])){
-    $user=$_SESSION['user'];
+if (isset($_SESSION['user'])) {
+    $user = $_SESSION['user'];
     $auth_status = include_template('auth_user.php', ['user_name' => $user['user-name'], 'avatar' => $user_avatar]);
+}
+
+if ($con) {
+    $sql_cat = 'SELECT id, category FROM categories ORDER BY id ASC';
+    $result = mysqli_query($con, $sql_cat);
+    $cat = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
 }
 
 $main_menu = '';
 foreach ($cat as $key => $value):
-    $main_menu .= include_template('main_menu_category.php', ['key' => $key, 'category' => $value]);
+    $main_menu .= include_template('main_menu_category.php', ['key' => $value['id'], 'category' => $value['category']]);
 endforeach;
 
 $list_menu = '';
 foreach ($cat as $key => $value):
-    $list_menu .= include_template('nav_list_category.php', ['category' => $value]);
+    $list_menu .= include_template('nav_list_category.php', ['category' => $value['category']]);
 endforeach;
 
 if (isset($_GET['category'])) {
--- init.php
+++ init.php
@@ -6,12 +6,10 @@ $con = mysqli_connect('localhost', 'root', '', 'yeticave');
 mysqli_set_charset($con, "utf8");
 
 if ($con == false) {
-    $error['db_error'] = 'Ошибка подключения: ' . mysqli_connect_error();
-    $page_content = include_template('error.php', ['error' => $error['db_error']]);
+    $error = 'Ошибка подключения: ' . mysqli_connect_error();
+    $page_content = include_template('error.php', ['error' => $error]);
     $layout_content = include_template('layout.php', ['page_title' => 'Войти', 'content' => $page_content, 'list_menu' => $list_menu, 'auth_status' => $auth_status]);
     $layout_content = preg_replace('<main class="container">', 'main', $layout_content);
     print($layout_content);
     exit;
-} else {
-    $result['db_connected'] = 'Соединение установлено';
 }
